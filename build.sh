6a31282eddcceb06637a74b2a91bbabef8eaae72
perseus.yang <ry82@cornell.edu>
2022-03-03 19:03:24 -0500

    beautiful

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 # zip the entire folder and deploy to lambda?
 zip -r deployment.zip src &>/dev/null
 # deploy the deployment file and upload to aws
-# ra 
-aws lambda update-function-code --function-name costack-auto-test --zip-file fileb://deployment.zip
+# ra
+aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://deployment.zip
 echo "deploy success"
 echo "what the heck"
