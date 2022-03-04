a1d08010a5a73ab2302feb6eb13dbbf375622858
perseus.yang <ry82@cornell.edu>
2022-03-04 17:57:20 -0500

    add main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # create your main function here
 # run the necessary operaitons and return
 import json
-def entropy_main(event, context):
+def test_main(event, context):
     # the input event is in a json string
     if type(event)==str:
         event = json.loads(event)
