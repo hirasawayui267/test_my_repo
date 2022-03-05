f43c69824afe60eb2d32919ebf3a5d870d2d7559
perseus.yang <ry82@cornell.edu>
2022-03-04 22:33:14 -0500

    ra

---
+++ rara.py
@@ -0,0 +1,10 @@
+# create your main function here
+# run the necessary operaitons and return
+import json
+def ra_main(event, context):
+    # the input event is in a json string
+    if type(event)==str:
+        event = json.loads(event)
+    # this is to make a new change
+    # update the code in the main function
+    return {"response": "this is my entropy function" }
