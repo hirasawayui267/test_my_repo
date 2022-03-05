8785b38e7b7aeeb365f5c5b33c6f143dfb8b849a
perseus.yang <ry82@cornell.edu>
2022-03-05 12:50:27 -0500

    ra

--- rara.py
+++ rara.py
@@ -6,5 +6,5 @@ def ra_main(event, context):
     if type(event)==str:
         event = json.loads(event)
     # this is to make a new change
-    # update the code in the main function
+    # update the code in the main function--testanother
     return {"response": "this is my entropy function" }
